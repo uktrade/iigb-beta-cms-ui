717dd6883391ac843c1baeba1e5b75f56e948f26
breneser <brhneser@gmail.com>
2016-10-20 17:06:20 +0100

    Added prose

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all:
-	babel lib --out-dir dist
-	node-sass lib/react-ui-tree.scss dist/react-ui-tree.css
-	webpack -p
+	./node_modules/babel-cli/bin/babel.js lib --out-dir dist
+	./node_modules/node-sass/bin/node-sass lib/react-ui-tree.scss dist/react-ui-tree.css
+	./node_modules/webpack/bin/webpack.js -p
 clean:
 	rm dist/*
 	rm example/bundle*
---
+++ server.js
@@ -0,0 +1,19 @@
+var 	express		= require('express'),
+        router          = express.Router(),
+    	app			= express(),
+    	port        = process.env.PORT || 3000;
+
+
+//some basic redirect routes
+//app.get('/', function(req, res) {
+//  res.redirect('prose/');
+//});
+
+app.set('case sensitive routing', false);
+
+app.use('/', express.static(__dirname + '/example'));
+app.use('/prose', express.static(__dirname + '/prose'));
+
+app.listen(port, function() {
+    console.log('listening');
+});
