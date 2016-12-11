b2144aeb8db06daefb16bb790531965f0e193c20
Aaron Witter <aaronwitter@hotmail.com>
2016-12-11 20:59:51 +0000

    update to js

--- dit-cms/src/auth/index.js
+++ dit-cms/src/auth/index.js
@@ -6,9 +6,10 @@
 
 // URL and endpoint constants
 const API_URL = 'http://localhost:3001/'
-const LOGIN_URL = API_URL + 'sessions/create/'
-const SIGNUP_URL = API_URL + 'users/'
-
+const LOGIN_URL = 'https://github.com/login/oauth/authorize'
+const CLIENT_ID = '14e4bbb060c1924d9d78' //TODO move to server
+const CALLBACK_URI = 'http://localhost:8080/callback' //TODO move to server
+  // const SIGNUP_URL = API_URL + 'users/'
 export default {
 
   // User object will let us check authentication status
@@ -17,20 +18,38 @@ export default {
   },
 
   // Send a request to the login URL and save the returned JWT
-  login(context, creds, redirect) {
-    context.$http.post(LOGIN_URL, creds, (data) => {
-      localStorage.setItem('id_token', data.id_token)
+  // login(context, redirect) {
+  //   context.$http.get(LOGIN_URL, (data) => {
+  //     console.log(data)
+  //     localStorage.setItem('id_token', data.id_token)
 
-      this.user.authenticated = true
+  //     this.user.authenticated = true
 
-      // Redirect to a specified route
-      if (redirect) {
-        router.go(redirect)
-      }
+  //     // Redirect to a specified route
+  //     if (redirect) {
+  //       router.go(redirect)
+  //     }
 
-    }).error((err) => {
-      context.error = err
-    })
+  //   }).error((err) => {
+  //     context.error = err
+  //   })
+  // },
+  // 
+  // 
+  login() {
+    window.location = LOGIN_URL + '?client_id=' + CLIENT_ID + '&redirect_uri=' + CALLBACK_URI;
+  },
+
+  authenticate(context, code) {
+    console.log("renn")
+    console.log(context)
+    context.$http
+      .get('http://iigb-beta-cms-gatekeeper.herokuapp.com/authenticate/' + code).then((data) => {
+          console.log(data);
+        },
+        (data) => {
+
+        })
   },
 
   signup(context, creds, redirect) {
--- dit-cms/src/components/Login.vue
+++ dit-cms/src/components/Login.vue
@@ -1,57 +1,56 @@
   <!-- src/components/Login.vue -->
 
   <template>
-  
-    <div class="col-sm-4 col-sm-offset-4">
+    <div class="row">
+    <div class="col-sm-4">
       <h2>Log In</h2>
-      <p>Log in to your account to get some great quotes.</p>
       <div class="alert alert-danger" v-if="error">
         <p>{{ error }}</p>
       </div>
-      <div class="form-group">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Enter your username"
-          v-model="credentials.username"
-        >
-      </div>
-      <div class="form-group">
-        <input
-          type="password"
-          class="form-control"
-          placeholder="Enter your password"
-          v-model="credentials.password"
-        >
-      </div>
-      <button class="btn btn-primary" @click="submit()">Access</button>
+      <button class="btn btn-primary" @click="submit()">Login via Github</button>
+      <button class="btn btn-primary" @click="authenticate()">Authenticate via Gatekeeper</button>
+    </div>
     </div>
   </template>
 
   <script>
   import auth from '../auth'
+  var Vue = require('vue');
+  var VueResource = require('vue-resource');
+
+  Vue.use(VueResource);
+
   export default {
     data() {
       return {
         // We need to initialize the component with any
         // properties that will be used in it
-        credentials: {
-          username: '',
-          password: ''
-        },
+        // credentials: {
+        //   username: '',
+        //   password: ''
+        // },
         error: ''
       }
     },
     methods: {
       submit() {
-        var credentials = {
-          username: this.credentials.username,
-          password: this.credentials.password
-        }
+        console.log(this, window.location.pathname)
+        // var credentials = {
+        //   username: this.credentials.username,
+        //   password: this.credentials.password
+        // }
         // We need to pass the component's this context
         // to properly make use of http in the auth service
-        auth.login(this, credentials, 'secretquote')
-      }
+        auth.login()
+      },
+      authenticate(){
+      
+      if (window.location.href.match(/\?code=(.*)/) && window.location.href.match(/\?code=(.*)/)[1]){
+      var code = window.location.href.match(/\?code=(.*)/)[1];
+      console.log("here");
+      auth.authenticate(this,code)
+    }
+    }
     }
 
   }
