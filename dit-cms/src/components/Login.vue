ac44f8be68e30be693d935e5ed81c051a19b6700
Aaron Witter <aaronwitter@hotmail.com>
2016-12-12 15:17:44 +0000

    updated login component

--- dit-cms/src/components/Login.vue
+++ dit-cms/src/components/Login.vue
@@ -7,51 +7,56 @@
       <div class="alert alert-danger" v-if="error">
         <p>{{ error }}</p>
       </div>
+      <div v-if="!authenticated">
       <button class="btn btn-primary" @click="submit()">Login via Github</button>
-      <button class="btn btn-primary" @click="authenticate()">Authenticate via Gatekeeper</button>
+      </div>
+      <div v-if="authenticated">
+      Logged In
+      <br>
+      <button class="btn btn-primary" @click="logout()">Logout</button>
+      </div>
     </div>
     </div>
   </template>
 
   <script>
   import auth from '../auth'
+
   var Vue = require('vue');
   var VueResource = require('vue-resource');
-
   Vue.use(VueResource);
 
   export default {
     data() {
       return {
-        // We need to initialize the component with any
-        // properties that will be used in it
-        // credentials: {
-        //   username: '',
-        //   password: ''
-        // },
+        authenticated: auth.checkAuth(),
         error: ''
       }
     },
+     mounted() {
+      if (this.authenticated == false && window.location.href.match(/\?code=(.*)/)[1]){
+        console.log("mounted");
+        var code = window.location.href.match(/\?code=(.*)/)[1];
+        this.authenticate(code);
+      }
+     },
     methods: {
       submit() {
-        console.log(this, window.location.pathname)
-        // var credentials = {
-        //   username: this.credentials.username,
-        //   password: this.credentials.password
-        // }
-        // We need to pass the component's this context
-        // to properly make use of http in the auth service
-        auth.login()
+        auth.login();
       },
-      authenticate(){
-      
-      if (window.location.href.match(/\?code=(.*)/) && window.location.href.match(/\?code=(.*)/)[1]){
-      var code = window.location.href.match(/\?code=(.*)/)[1];
-      console.log("here");
-      auth.authenticate(this,code)
+      authenticate(code){
+        auth.authenticate(this,code);
+        setTimeout(
+          function(){ if(auth.checkAuth() == true){
+          location.reload()
+          }}
+          ,1000);
+      },
+      logout(){
+        auth.logout();
+        setTimeout(function(){location.reload()},500);
+      }
     }
-    }
-    }
-
   }
+
   </script>
