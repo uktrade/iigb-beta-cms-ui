48325b485b0da7cfdb759bdaf5cda9572c0e4200
pastorjuradotraverso <pastor.methods@gmail.com>
2017-01-03 18:20:52 +0000

    Chang proportions to keep edited and compiled text together

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -72,16 +72,27 @@
     margin: 0;
     height: 460px;
     font-family: 'Helvetica Neue', Arial, sans-serif;
+    font-size: 15px;
     color: #333;
   }
 
   textarea, #editor div {
     display: inline-block;
-    width: 49%;
+    width: 55%;
     height: 100%;
     vertical-align: top;
     box-sizing: border-box;
     padding: 0 20px;
+  }
+
+  #editor div {
+    display: inline-block;
+    width: 44%;
+    height: 100%;
+    vertical-align: top;
+    box-sizing: border-box;
+    padding: 0 20px;
+    overflow: scroll;
   }
 
   textarea {
