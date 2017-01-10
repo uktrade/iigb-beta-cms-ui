0c655e7687bb33e1d5009738fc108678b67946d7
breneser <brhneser@gmail.com>
2017-01-10 10:23:25 +0000

    Add loadContent function to github module

--- src/github/index.js
+++ src/github/index.js
@@ -10,11 +10,12 @@ var github = new GitHub({
 var structures = github.getRepo(conf.structure.org, conf.structure.repo);
 var media = github.getRepo(conf.content.org, conf.content.repo);
 var sitePattern = new RegExp(".+?_.+?.json");
+var contents = github.getRepo(conf.content.org, conf.content.repo);
 
 export default {
   //load structure file list
   loadSites() {
-    return loadContent(conf.structure.path)
+    return loadStructure(conf.structure.path)
       .then(function(data) {
         var files = data || [];
         var sites = [];
@@ -30,7 +31,7 @@ export default {
       });
   },
   loadSite(path) {
-    return loadContent(path);
+    return loadStructure(path);
   },
   /*
    *  Update site content
@@ -76,10 +77,22 @@ export default {
   },
   getMediaRoot() {
     return conf.content.mediaPath;
+  },
+  loadContent(path) {
+    var _path = conf.content.path + '/' + path;
+    return contents
+      .getContents(
+        conf.content.dev,
+        _path,
+        true
+      )
+      .then(function(response){
+        return response.data;
+      });
   }
 };
 
-function loadContent(path) {
+function loadStructure(path) {
   return structures
     .getContents(
       conf.structure.dev,
